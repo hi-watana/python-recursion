13dc8ffdbc06601cd308d59440a3f80afbdc8177
hi-watana <hero95alive@gmail.com>
2021-10-16 14:05:39 +0900

    Added fact

--- recursive_functions.rb
+++ recursive_functions.rb
@@ -1,3 +1,8 @@
+def fact(n)
+  # TODO: Implement
+  return 0
+end
+
 def fib(n)
   # TODO: Implement
   return 0
--- test_recursive_functions.rb
+++ test_recursive_functions.rb
@@ -1,7 +1,15 @@
 require 'test/unit'
 require './recursive_functions'
 
-class TestFib < Test::Unit::TestCase
+class TestRecursiveFunctions < Test::Unit::TestCase
+  def test_fact_n_is_0
+    assert_equal(1, fact(0))
+  end
+
+  def test_fact_n_is_6
+    assert_equal(720, fact(6))
+  end
+
   def test_fib_n_is_0
     assert_equal(1, fib(0))
   end
