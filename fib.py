39c9cb5379b9b46f3b74db7362cfe0c704e24398
hi-watana <hero95alive@gmail.com>
2021-10-17 15:35:20 +0900

    Implemented

--- fib.py
+++ fib.py
@@ -1,3 +1,7 @@
 def fib(n):
-    # TODO: Implement this
-    return 0
+    def _fib(k, a0, a1):
+        while True:
+            if k <= 1:
+                return a1
+            k, a0, a1 = k - 1, a1, a0 + a1
+    return _fib(n, 1, 1)
