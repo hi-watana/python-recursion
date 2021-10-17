2ebf5b64e2a417ee2641781cfec22d0eedb4390b
hi-watana <hero95alive@gmail.com>
2021-10-17 15:01:56 +0900

    Implemented

--- fact.py
+++ fact.py
@@ -1,3 +1,6 @@
 def fact(n):
-    # TODO: Implement this
-    return 0
+    def _fact(k, cont):
+        if k <= 0:
+            return cont(1)
+        return _fact(k - 1, lambda x: cont(x * k))
+    return _fact(n, lambda x: x)
