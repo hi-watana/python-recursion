a97844864b7f799b7a44bb9ee26e7763153614c9
hi-watana <hero95alive@gmail.com>
2021-10-17 15:21:22 +0900

    Implemented

--- fib.py
+++ fib.py
@@ -1,3 +1,10 @@
 def fib(n):
-    # TODO: Implement this
-    return 0
+    memo = [0] * (n + 1)
+    def _fib(k):
+        if k <= 1:
+            return 1
+        if memo[k] > 0:
+            return memo[k]
+        memo[k] = _fib(k - 1) + _fib(k - 2)
+        return memo[k]
+    return _fib(n)
